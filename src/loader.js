6d4d32fe77d1f09c3496e6af2a9f3a3262b5b7de
Freddie Ridell <freddie@codogo.io>
2017-06-06 00:30:31 +0100

    feat(loader): running overnight on a crontab to see how it goes

--- src/loader.js
+++ src/loader.js
@@ -1,3 +1,8 @@
+require("source-map-support").install();
+require("babel-register")({
+	sourceMaps: true,
+});
+
 import shitFetch from "node-fetch";
 import R from "ramda";
 import mysql from "mysql";
@@ -10,26 +15,28 @@ const connection = mysql.createConnection({
 	password: "",
 });
 
-pairs.forEach(([lhs, rhs,]) =>
-	shitFetch(`https://api.cryptonator.com/api/ticker/${lhs}-${rhs}`)
-		.then(x => x.json())
-		.then(({ ticker, timestamp, }) => ({
-			timestamp,
-			rate: parseFloat(R.prop("price")(ticker), 10),
-		}))
-		.then(({ timestamp, rate, }) =>
-			connection.query(
-				{
-					sql: "INSERT INTO crypto_tracker.rates  VALUES ( ?, ?, ?, ? );",
-					timeout: 40000, // 40s
-					values: [timestamp, rate, lhs, rhs,],
-				},
-				err => (err ? console.err(err) : null),
+Promise.all(
+	pairs.map(([lhs, rhs,]) =>
+		shitFetch(`https://api.cryptonator.com/api/ticker/${lhs}-${rhs}`)
+			.then(x => x.json())
+			.then(({ ticker, timestamp, }) => ({
+				timestamp,
+				rate: parseFloat(R.prop("price")(ticker), 10),
+			}))
+			.then(({ timestamp, rate, }) =>
+				connection.query(
+					{
+						sql: "INSERT INTO crypto_tracker.rates  VALUES ( ?, ?, ?, ? );",
+						timeout: 40000, // 40s
+						values: [timestamp, rate, lhs, rhs,],
+					},
+					err => (err ? console.error(err) : null),
+				),
 			),
-		),
+	),
+).then(() =>
+	connection.end(function(err) {
+		console.log("DONE SQL");
+		err && console.error(err);
+	}),
 );
-
-connection.end(function(err) {
-	console.log("DONE SQL");
-	err && console.error(err);
-});
