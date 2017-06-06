785cf8b7845da629256f0dd3d2d3808d13c4032c
Freddie Ridell <freddie@codogo.io>
2017-06-06 22:01:05 +0100

    feat(interactive-email): added refference code for interactive email

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+//<style>
+//input,
+//.hide,
+//.content,
+//input:checked ~ .show{
+//display:none;
+//}
+//input:checked ~ .hide,
+//input:checked ~ .content{
+//display:block;
+//}
+//</style>
+//<label>
+//<input type=”checkbox”>
+//<div class=”show”>show</div>
+//<div class=”hide”>hide</div>
+//<label class=”content”>…</label>
+//</label>
