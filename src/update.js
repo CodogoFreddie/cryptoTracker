fbc9e081a8058f771bb432693299a2bf3e6d421e
Freddie Ridell <freddie@codogo.io>
2017-06-06 21:47:14 +0100

    feat(update): added updating script that  is run on the server  once a day

---
+++ src/update.js
@@ -0,0 +1,4 @@
+//current crontab:
+//*       1       *       *       *       /home/freddie/cryptoTracker/runYarnFromCron.sh run update >> /tmp/update.debug.txt
+//*/10    *       *       *       *       /home/freddie/cryptoTracker/runYarnFromCron.sh run fetch >> /tmp/fetch.debug.txt
+//*       5       *       *       *       /home/freddie/cryptoTracker/runYarnFromCron.sh run generate >> /tmp/generate.debug.txt
