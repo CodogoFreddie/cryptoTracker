5a6407644bba3335f7d314d2c0203420e8fa3bec
Freddie Ridell <freddie@codogo.io>
2017-06-11 22:29:00 +0100

    feat(pairs): simple pair-gen system

--- src/pairs.js
+++ src/pairs.js
@@ -1,3 +1,5 @@
+import R from "ramda";
+
 export default [
 	["btc", "gbp",],
 	["eth", "gbp",],
@@ -9,3 +11,16 @@ export default [
 	["btc", "xmr",],
 	["eth", "xmr",],
 ];
+
+const currencies = ["btc", "eth", "xmr", "etc", "gbp",];
+
+console.log(
+	R.flatten(
+		currencies.map((lhs, i) =>
+			currencies.slice(i + 1, Infinity).map(rhs => ({
+				lhs,
+				rhs,
+			})),
+		),
+	),
+);
